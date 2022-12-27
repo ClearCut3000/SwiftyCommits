4af4cd3824fe2015702c9e2edf325f316cdea8f3
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-12-27 23:23:00 +0300

    Parsing JSON

--- SwiftyCommits/SCApp.m
+++ SwiftyCommits/SCApp.m
@@ -19,6 +19,25 @@
     NSLog(@"Fatal error 1: %@", [error localizedDescription]);
     exit(0);
   }
+  //Data Decoding
+  NSArray *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+  // In case of error
+  if (error != nil) {
+    NSLog(@"Fatal error 1: %@", [error localizedDescription]);
+    exit(0);
+  }
+  // Loop through each array dictionary
+  for (NSDictionary *entry in json) {
+    //pull data into var's
+    NSString *name = entry[@"commit"][@"author"][@"name"];
+    NSString *message = entry[@"commit"][@"message"];
+
+    //remove linebraks
+    message = [message stringByReplacingOccurrencesOfString:@"\n" withString:@" "];
+    // print all parsed data
+    printf("%s: %s\n\n", [name cStringUsingEncoding:NSUTF8StringEncoding], [message cStringUsingEncoding:NSUTF8StringEncoding]);
+    
+  }
 }
 
 @end
