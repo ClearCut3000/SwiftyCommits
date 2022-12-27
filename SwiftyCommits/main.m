5fb3296e5a3a64bef9c902969a1689196eadae8c
NIKOLAY NIKITIN <nikitin.nikolay.v@gmail.com>
2022-12-27 22:50:57 +0300

    Initial Commit

---
+++ SwiftyCommits/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SwiftyCommits
+//
+//  Created by Николай Никитин on 27.12.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+  @autoreleasepool {
+      // insert code here...
+      NSLog(@"Hello, World!");
+  }
+  return 0;
+}
